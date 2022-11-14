d0433d954b8b2252849f6b159ed2e7603da1ffce
lmy9237 <lmy9237@naver.com>
2022-11-14 19:39:58 +0900

    첫 커밋

---
+++ src/day02/test/MainClass01.java
@@ -0,0 +1,11 @@
+package day02.test;
+// main 스페이스하면 기본형식나옴
+// syso 를 치면 찍어볼 수 있다
+
+public class MainClass01 {
+	// 프로그램이 시작이 되는 메인메소드
+	public static void main(String[] args) {
+		System.out.println("메인메소드 시작!");
+	}
+	
+}
